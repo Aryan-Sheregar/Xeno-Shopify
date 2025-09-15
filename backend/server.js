94ef3cda182d0c64a39c1e0207ccff9508d16ca9
Aryan <72085890+Aryan-Sheregar@users.noreply.github.com>
2025-09-15 22:22:22 +0530

    Final commit

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://xeno-shopify-chi.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Tenant-ID"],
